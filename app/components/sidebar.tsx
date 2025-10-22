c32b1ed7fe239179d35da8aa46cf7e0615548074
Rasmus Jensen <raje@dbc.dk>
2025-10-22 13:50:06 +0200

    only edit custom masks

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -249,12 +249,9 @@ export function SideBar(props: { className?: string }) {
   const currentSystemPrompt = process.env.NEXT_PUBLIC_SYSTEM_PROMPT_IN_SIDEBAR
     ? currentChat?.mask?.context?.[0]?.content
     : undefined;
+  const editableSystemPrompt = !currentChat?.mask?.builtin;
   const handlePromptChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     chatStore.updateTargetSession(currentChat, (session) => {
-      if (session.mask.builtin) {
-        session.mask = maskStore.create(session.mask);
-        session.mask.name = "Tilpasset " + session.mask.name;
-      }
       session.mask.context[0].content = e.target.value;
     });
   };
@@ -275,8 +272,26 @@ export function SideBar(props: { className?: string }) {
       >
         {currentSystemPrompt !== undefined && !shouldNarrow && (
           <div className={styles["system-prompt-preview"]}>
-            <div className={styles["system-prompt-label"]}>Systemprompt:</div>
+            <div className={styles["system-prompt-label"]}>
+              Systemprompt
+              {!editableSystemPrompt && (
+                <div
+                  style={{
+                    color: "gray",
+                    fontSize: "80%",
+                    fontWeight: "normal",
+                    marginBottom: "8px",
+                  }}
+                >
+                  Indbygget assistent kan ikke ændres. Opret ny assistent for at
+                  redigere.
+                </div>
+              )}
+            </div>
+
             <textarea
+              style={{ opacity: editableSystemPrompt ? 1 : 0.7 }}
+              disabled={!editableSystemPrompt}
               value={currentSystemPrompt as string}
               onChange={handlePromptChange}
               className={styles["system-prompt-textarea"]}
